0a8362f1071085597ca34f8bdd52b2f3b1a57874
Jia <vernalin1422@gmail.com>
2023-01-05 09:52:54 +0800

    更新

--- CalChicken/Calculate.cs
+++ CalChicken/Calculate.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace CalChicken
 {
-    public class Calculate
+	//題目:公雞5元1隻 母雞3元1隻 小雞1元3隻 用一百元買一百隻雞
+	public class Calculate
     {
-        
-    }
+		
+		/// <summary>
+		/// 計算百元買百機的所有可能性
+		/// 用責任練解題
+		/// </summary>
+		/// <returns>傳回每種雞的數量</returns>
+		public static string HowManyChicken()
+		{
+			string result = string.Empty;
+			int totalPrice = 100; //總金額
+			int Num = 100; //總數量
+			int cockPrice = 5; //公雞金額
+			int henPrice = 3; //母雞金額
+			int chickPrice = 1; //小雞金額
+			int chick; //小雞數量
+			int chickgroup = 3; //小雞一組3隻
+
+			for (int cock = 0; cock <= Num/cockPrice; cock++)//公雞一隻五元 最多20組
+			{
+				for (int hen = 0; (cockPrice * cock)+(henPrice * hen) <= totalPrice; hen++)//母雞一隻三元 最多33組
+				{
+					chick = Num - cock - hen;
+					if (chick % chickgroup == 0 &&(cockPrice * cock) + (henPrice * hen) + (chickPrice * chick / chickgroup) == totalPrice)
+						result += $"{cock}隻公雞,{hen}隻母雞,{chick}隻小雞\n";
+				}
+			}
+
+			return result;
+		}
+	}
 }
--- Chicken/Program.cs
+++ Chicken/Program.cs
@@ -11,19 +11,23 @@ namespace Chicken
 		static void Main(string[] args)
 		{
 
-			Console.WriteLine(Calculate.HowManyChicken());
+			Console.WriteLine(CalChicken.Calculate.HowManyChicken());
 		}
 	}
 	public class Calculate
-	{
+	{/// <summary>
+	/// 計算百元買百機的所有可能性
+	/// 用責任練解題
+	/// </summary>
+	/// <returns>傳回每種雞的數量</returns>
 		public static string HowManyChicken()
 		{
 			string result =string.Empty;
-			for(int cock = 0; cock <=20; cock++)//一隻五元 最多20組
+			for(int cock = 0; cock <=20; cock++)//公雞一隻五元 最多20組
 			{
-				for(int hen = 0; hen <= 33; hen++)//一隻三元 最多33組
+				for(int hen = 0; hen <= 33; hen++)//母雞一隻三元 最多33組
 				{
-					for (int chick = 0; chick <= 33; chick++)//一元三隻算一組 最多33組
+					for (int chick = 0; chick <= 33; chick++)//小雞一元三隻算一組 最多33組
 					{
 						if (cock + hen + (chick * 3) == 100 && (5 * cock) + (3 * hen) + chick == 100)
 							result += $"{cock}隻公雞,{hen}隻母雞,{chick * 3}隻小雞\n";
