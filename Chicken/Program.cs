2221bb0bf2ccd04a38aa8290c48f71a1544b6dae
Jia <vernalin1422@gmail.com>
2023-01-05 09:01:40 +0800

    加註解

--- Chicken/Program.cs
+++ Chicken/Program.cs
@@ -19,11 +19,11 @@ namespace Chicken
 		public static string HowManyChicken()
 		{
 			string result =string.Empty;
-			for(int cock = 1; cock <=20; cock++)
+			for(int cock = 0; cock <=20; cock++)//一隻五元 最多20組
 			{
-				for(int hen = 1; hen <= 33; hen++)
+				for(int hen = 0; hen <= 33; hen++)//一隻三元 最多33組
 				{
-					for (int chick = 1; chick <= 33; chick++)
+					for (int chick = 0; chick <= 33; chick++)//一元三隻算一組 最多33組
 					{
 						if (cock + hen + (chick * 3) == 100 && (5 * cock) + (3 * hen) + chick == 100)
 							result += $"{cock}隻公雞,{hen}隻母雞,{chick * 3}隻小雞\n";
